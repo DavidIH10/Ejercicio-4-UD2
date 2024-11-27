7a3910d77ed52bd69ad60e99edf4c01ba1e900e0
DavidIH10 <24.damt.davidiulianherman@fundacionmontessori.com>
2024-11-28 00:18:27 +0100

    Añado proyecto1 y .gitignore

---
+++ Proyecto1/src/area/AREA.java
@@ -0,0 +1,25 @@
+package area;
+
+import java.util.Scanner;
+
+public class AREA {
+
+	public AREA() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+Scanner scanner = new Scanner(System.in);
+        
+        System.out.print("Introduce el radio del círculo: ");
+        double radio = scanner.nextDouble();
+        
+        double area = Math.PI * Math.pow(radio, 2);
+        System.out.println("El área del círculo es: " + area);
+        
+        scanner.close();
+
+	}
+
+}
